f0b5fe42816216012250bd74a042afb760266aad
Cvinoba <33060958+Cvinoba@users.noreply.github.com>
2018-01-31 19:05:03 +0530

    Create vino.java

---
+++ vino.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class vino
+{
+public static void main(String s[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n<0)
+{
+System.out.print("negative");
+}
+else if(n>0)
+{
+System.out.print("positive");
+}
+else
+{
+System.out.print("zero");
+}
+}
+}
